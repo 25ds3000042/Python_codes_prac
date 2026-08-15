780131534e8b1177618f2cc1bd13467c446ff9e0
bss1211 <mail.bhupendra.sidar.com>
2026-08-15 21:36:11 +0530

    OOP_all_varients

---
+++ Conditions/elif.py
@@ -0,0 +1,12 @@
+x = print(int("enter number a number = "))
+
+def main():
+    if(x > o):
+        print("x")
+
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ Dictionaries/nested_dict.py
@@ -0,0 +1,15 @@
+company = {
+    "employee": {
+        "personal": {
+            "name": "Owl",
+            "age": 25
+        },
+        "job": {
+            "title": "Python Developer",
+            "salary": 50000
+        }
+    }
+}
+
+print(company["employee"]["personal"]["name"])
+print(company["employee"]["job"]["title"])
